a2e8ef00c7719dd4f2c4734a416f4526bced9d26
Ganesh M <ganeshm24122006@gmail.com>
2026-05-13 18:25:56 +0530

    chore: remove .next from git cache

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"RzXQmWLMNGpM25h+g52cKQf+vo18FlmiHR0cpnNnYBM=\"\n}"
--- .next/dev/static/chunks/src_app_page_tsx_05bh_3n._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules_0k_t~j5._.js"],
-    source: "dynamic"
-});
